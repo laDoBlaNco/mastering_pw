86173c60ac2225c22f5c8b3bfc43247bf9f7d264
ladoblanco <whitesidekevin@gmail.com>
2026-09-02 01:18:07 -0400

    Redoing pwUIMastery Module as it flaked out

--- pw-ui-mastery/js-fundamentals/lessons/lesson9.js
+++
@@ -1,8 +0,0 @@
-import { customerDetails } from "../helper/printHelper.js";
-
-// let customerDetails = new CustomerDetails() // instance of our class
-// we can also use the singleton pattern in js modules and move this instantiation
-// to our helper file
-
-customerDetails.printFirstName('Kelen')
-customerDetails.printLastName('Whiteside')
